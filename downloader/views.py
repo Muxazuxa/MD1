4a448ead287c964ef0eaa8a478758b3cc37cefd8
jenish <muxazuxa@mail.ru>
2019-05-21 15:35:19 +0600

    Adding directory for videos

--- downloader/views.py
+++ downloader/views.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .forms import DownloadForm
 import youtube_dl
-
+from .models import History
+import os
 # Create your views here.
 
 
@@ -12,7 +13,17 @@ def download(request):
         url = form.cleaned_data.get('url')
         options = {'format': 'best'}
         with youtube_dl.YoutubeDL(options) as youtube:
-            info = youtube.extract_info(url)
+            dir = os.getcwd()
+            if not os.path.exists(dir + '/movies'):
+                os.makedirs(dir + '/movies')
+                os.chdir(dir + '/movies')
+                info = youtube.extract_info(url)
+                h = History(url=url)
+                h.save()
+                os.chdir(dir)
+            # response = HttpResponse(content_type='application/force-download')
+            # response['Content-Disposition'] = 'attachment; filename="{}.{}"'.format(info['title'], info['ext'])
+            # return response
         return render(request, 'downloader/index.html', {'form': form, 'success': '{} was successfully downloaded'.format(info['title'])})
     return render(request, 'downloader/index.html', {'form': form})
 
